f5be69bc165eb2e8e701eec48ae5a697ac6c8409
CodeBardian <jacksw247@gmail.com>
2020-12-18 11:41:50 +0100

    add dimmer class

---
+++ tplink-smartdevices/Data/DimmerOptions.cs
@@ -0,0 +1,29 @@
+﻿namespace TPLinkSmartDevices.Devices
+{
+    internal class DimmerOptions
+    {
+        public DimmerOptions()
+        {
+            FadeOnTime = 2000;
+            FadeOffTime = 2000;
+            GentleOnTime = 2000;
+            GentleOffTime = 2000;
+            Mode = DimmerMode.GentleOnOff;
+        }
+
+        public int FadeOnTime { get; set; }
+        public int FadeOffTime { get; set; }
+        public int GentleOnTime { get; set; }
+        public int GentleOffTime { get; set; }
+
+        public DimmerMode Mode { get; set; }
+    }
+
+    public enum DimmerMode
+    {
+        GentleOnOff,
+        InstantOnOff,
+        Preset,
+        None
+    }
+}
---
+++ tplink-smartdevices/Devices/TPLinkSmartDimmer.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPLinkSmartDevices.Devices
+{
+    class TPLinkSmartDimmer : TPLinkSmartDevice
+    {
+        private DimmerOptions _options;
+
+        [Obsolete("Use async factory method TPLinkSmartDimmer.Create() instead")]
+        public TPLinkSmartDimmer(string hostName, int port = 9999) : base(hostName, port)
+        {
+            Task.Run(async () => await Refresh()).GetAwaiter().GetResult();
+        }
+
+        private TPLinkSmartDimmer() { }
+
+        public static async Task<TPLinkSmartDimmer> Create(string hostname, int port = 9999, DimmerOptions opts = null)
+        {
+            var d = new TPLinkSmartDimmer() { Hostname = hostname, Port = port };
+            d._options = opts ?? new DimmerOptions();
+            await d.Refresh().ConfigureAwait(false);
+            return d;
+        }
+
+        public async Task Refresh()
+        {
+            dynamic sysInfo = await Execute("system", "get_sysinfo").ConfigureAwait(false);
+
+            await RetrievePresets();
+            await Refresh((object)sysInfo).ConfigureAwait(false);
+        }
+
+        private Task RetrievePresets()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
