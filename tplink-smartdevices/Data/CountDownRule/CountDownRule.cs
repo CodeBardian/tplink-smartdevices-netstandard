91294b57db4a81e70416b6b9ed33742e7c09df8a
CodeBardian <jacksw247@gmail.com>
2021-02-16 20:22:04 +0100

    add timer functionality for plugs

---
+++ tplink-smartdevices/Data/CountDownRule/CountDownRule.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TPLinkSmartDevices.Data.CountDownRule
+{
+    public class CountDownRule
+    {
+        /// <summary>
+        /// identifier of CountDown rule
+        /// </summary>
+        public string Id { get; set; }
+
+        /// <summary>
+        /// custom name of CountDown rule
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// if the rule is currently active or not
+        /// </summary>
+        public bool Enabled { get; set; }
+
+        /// <summary>
+        /// if the device should be powered on or off after the timer runs out
+        /// </summary>
+        public bool PoweredOn { get; set; }
+
+        /// <summary>
+        /// delay in seconds after which the action triggers 
+        /// </summary>
+        public int Delay { get; set; }
+    }
+}
--- tplink-smartdevices/Devices/TPLinkSmartPlug.cs
+++ tplink-smartdevices/Devices/TPLinkSmartPlug.cs
@@ -4,10 +4,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TPLinkSmartDevices.Data.CountDownRule;
 
 namespace TPLinkSmartDevices.Devices
 {
-    public class TPLinkSmartPlug : TPLinkSmartDevice
+    public class TPLinkSmartPlug : TPLinkSmartDevice, ICountDown
     {
         /// <summary>
         /// If the outlet relay is powered on
@@ -25,6 +26,7 @@ namespace TPLinkSmartDevices.Devices
         public DateTime PoweredOnSince { get; private set; }
 
         public string[] Features { get; private set; }
+        public List<CountDownRule> CountDownRules { get; private set; }
 
         public TPLinkSmartPlug(string hostname, int port = 9999) : base(hostname, port)
         {
@@ -61,6 +63,7 @@ namespace TPLinkSmartDevices.Devices
             else
                 PoweredOnSince = DateTime.Now - TimeSpan.FromSeconds((int)sysInfo.on_time);
 
+            await RetrieveCountDownRules().ConfigureAwait(false);
             await Refresh((object)sysInfo).ConfigureAwait(false);
         }
 
@@ -98,6 +101,91 @@ namespace TPLinkSmartDevices.Devices
         {
             await Execute("system", "set_led_off", "off", value ? 0 : 1).ConfigureAwait(false);
             this.LedOn = value;
+        }
+
+        public async Task RetrieveCountDownRules()
+        {
+            dynamic result = await Execute("count_down", "get_rules").ConfigureAwait(false);
+            JArray ruleList = JArray.Parse(Convert.ToString(result.rule_list));
+            CountDownRules = ruleList.Count != 0 ? ruleList.Select(x => new CountDownRule
+            {
+                Enabled = (bool)x["enable"],
+                Delay = (int)x["delay"], 
+                PoweredOn = (bool)x["act"],
+                Name = (string)x["name"],
+            }).ToList() : new List<CountDownRule>();
+        }
+
+        public async Task AddCountDownRule(CountDownRule cdr)
+        {
+            if (CountDownRules.Any(c => c.Id == cdr.Id)) throw new Exception("countdown rule with specified id already exists");
+
+            dynamic result = await Execute("count_down", "add_rule", new JObject
+            {
+                new JProperty("enable", cdr.Enabled),
+                new JProperty("delay", cdr.Delay),
+                new JProperty("act", cdr.PoweredOn),
+                new JProperty("name", cdr.Name ?? "countdown rule")
+            }, null).ConfigureAwait(false);
+            cdr.Id = (string)result.id;
+            CountDownRules.Add(cdr);
+        }
+
+        public async Task EditCountDownRule(string id, bool? enabled = null, int? delay = null, bool? poweredOn = null, string name = null)
+        {
+            CountDownRule cdr = CountDownRules.Find(c => c.Id == id);
+
+            if (cdr == null) throw new Exception("plug has no countdown rule with specified id");
+
+            cdr.Enabled = enabled ?? cdr.Enabled;
+            cdr.Delay = delay ?? cdr.Delay;
+            cdr.PoweredOn = poweredOn ?? cdr.PoweredOn;
+            cdr.Name = name ?? cdr.Name;
+
+            dynamic result = await Execute("count_down", "edit_rule", new JObject
+            {
+                new JProperty("id", id),
+                new JProperty("enable", cdr.Enabled),
+                new JProperty("delay", cdr.Delay),
+                new JProperty("act", cdr.PoweredOn),
+                new JProperty("name", cdr.Name) 
+            }, null).ConfigureAwait(false);
+        }
+
+        public async Task EditCountDownRule(CountDownRule newCdr)
+        {
+            if (newCdr.Id == null) throw new Exception("countdown rule id is required");
+            if (CountDownRules.Any(c => c.Id == newCdr.Id)) throw new Exception("countdown rule with specified id already exists");
+
+            CountDownRule cdr = CountDownRules.Find(c => c.Id == newCdr.Id);
+            cdr.Enabled = newCdr.Enabled;
+            cdr.Delay = newCdr.Delay;
+            cdr.PoweredOn = newCdr.PoweredOn;
+            cdr.Name = newCdr.Name ?? cdr.Name;
+
+            dynamic result = await Execute("count_down", "edit_rule", new JObject
+            {
+                new JProperty("id", cdr.Id),
+                new JProperty("enable", cdr.Enabled),
+                new JProperty("delay", cdr.Delay),
+                new JProperty("act", cdr.PoweredOn),
+                new JProperty("name", cdr.Name)
+            }, null).ConfigureAwait(false);
+        }
+
+
+        public async Task DeleteCountDownRule(string id)
+        {
+            dynamic result = await Execute("count_down", "delete_rule", "id", id).ConfigureAwait(false);
+
+            CountDownRules.RemoveAll(c => c.Id == id);
+        }
+
+        public async Task DeleteAllCountDownRules()
+        {
+            dynamic result = await Execute("count_down", "delete_all_rules").ConfigureAwait(false);
+
+            CountDownRules.Clear();
         }
     }
 }
